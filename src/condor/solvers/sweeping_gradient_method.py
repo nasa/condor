87ee124a2df626000f5ab108c83597d6a584a13a
Benjamin Margolis <benjamin.margolis@nasa.gov>
2026-01-18 07:33:06 -0800

    fix scipy==1.17.0 dopri max_step bug

--- src/condor/solvers/sweeping_gradient_method.py
+++ src/condor/solvers/sweeping_gradient_method.py
@@ -82,6 +82,7 @@ class SolverSciPyBase(SolverMixin):
         self.adaptive_min_steps = adaptive_min_steps
         self.separate_events = separate_events
         self.reset_step_after_event = reset_step_after_event
+        self.max_step_size = max_step_size
         self.solver = scipy_ode(
             system.dots,
         )
@@ -99,6 +100,10 @@ class SolverSciPyBase(SolverMixin):
                 self.root_options[k.replace("rootfinder_", "")] = v
             else:
                 self.int_options[k] = v
+
+        self.do_update_integrator_options = (
+            adaptive_min_steps or not reset_step_after_event or max_step_size == 0.0
+        )
 
         self.solver.set_integrator(**self.int_options)
         self.solver.set_solout(self.solout)
@@ -230,7 +235,10 @@ class SolverSciPyBase(SolverMixin):
                     dict(max_step=np.abs(next_t - last_t) / self.adaptive_min_steps)
                 )
 
-            if self.adaptive_min_steps or not self.reset_step_after_event:
+            if self.max_step_size == 0.0:
+                self.int_options["max_step"] = np.abs(next_t - last_t)
+
+            if self.do_update_integrator_options:
                 self.solver.set_integrator(**self.int_options)
                 self.solver.set_solout(self.solout)
 
